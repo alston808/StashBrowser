416debade6fb83751237a7ac3009b22429274eb8
alston808 <alston.albarado@gmail.com>
2025-11-04 15:43:56 -1000

    bun update

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,10 @@
   import { ApolloProvider } from "@apollo/client";
   import { apolloClient } from "./lib/apolloClient";
   import App from "./App.tsx";
-  import Favicon from "./components/Favicon";
   import "./index.css";
 
   createRoot(document.getElementById("root")!).render(
     <ApolloProvider client={apolloClient}>
-      <Favicon />
       <App />
     </ApolloProvider>
   );
